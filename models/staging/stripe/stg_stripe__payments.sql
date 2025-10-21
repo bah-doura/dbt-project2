e97cbf1472e82d037aa89867a1a5ce0de67dd4a6
Abdourahmane Bah <abdourahmanebah112@gmail.com>
2025-10-21 11:59:51 +0000

    Examplar: building the full model

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,17 @@
+
+
+
+select
+    id as payment_id,
+    order_id as order_id,
+    payment_method as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.jaffle_shop.stripe_payments
+
+
+
+
